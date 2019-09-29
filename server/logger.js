c4924d18b125060c78dc3029769ae1ed54ed6e73
Den <dlilienblum@gmail.com>
2019-09-29 20:26:45 +0300

    simple logger transport for production

--- server/logger.js
+++ server/logger.js
@@ -11,25 +11,25 @@ const logger = winston.createLogger({
 
 if (process.env.NODE_ENV !== 'production') {
   logger.level = 'silly'
-  const alignedWithColorsAndTime = winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp(),
-    winston.format.printf((info) => {
-      const {
-        timestamp, level, message, ...args
-      } = info
-
-      const ts = timestamp.slice(0, 19).replace('T', ' ')
-      const data = Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
-      return `${ts} [${level}]: ${message} ${data}`
-    })
-  )
-
   logger.add(new winston.transports.Console({
-    format: alignedWithColorsAndTime
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.printf((info) => {
+        const {
+          timestamp, level, message, ...args
+        } = info
+
+        const ts = timestamp.slice(0, 19).replace('T', ' ')
+        const data = Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
+        return `${ts} [${level}]: ${message} ${data}`
+      })
+    )
   }))
 } else {
-  // TODO: transport for Heroku
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }))
 }
 
 module.exports = logger
